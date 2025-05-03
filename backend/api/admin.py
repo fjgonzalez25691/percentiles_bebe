4c949313eda7e3feaeaa7433c7c65c278d185308
Francisco Javier González Aparicio <fjgonzalez25691@gmail.com>
2025-05-03 17:48:39 +0200

    Añadiendo campos al administrador

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Baby, Measurement, Percentile
+
+@admin.register(Baby)
+class BabyAdmin(admin.ModelAdmin):
+    # Listar todo los campos del modelo Baby en el panel de administración
+    list_display = [field.name for field in Baby._meta.fields]
+    
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    # Listar todo los campos del modelo Measurement en el panel de administración
+    list_display = [field.name for field in Measurement._meta.fields]
+    
+@admin.register(Percentile)
+class PercentileAdmin(admin.ModelAdmin):
+    # Listar todo los campos del modelo Percentile en el panel de administración
+    list_display = [field.name for field in Percentile._meta.fields]
+      
+    
